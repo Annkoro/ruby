caf1422335da9899e844ae19c978aaf7199630f5
EC2 Default User <ec2-user@ip-172-31-20-153.ec2.internal>
2022-01-07 14:04:20 +0000

    [Update]

---
+++ lesson6.rb
@@ -0,0 +1,8 @@
+total_price = "100円"
+if total_price > "100円"
+  puts "みかんを購入。所持金に余裕あり。"
+elsif total_price == "100円"
+  puts "みかんを購入。所持金は０円。"
+elsif total_price < "100円"
+  puts "みかんを購入することができません。"
+end
